03a28f96c23678ed4cfbfc18c343ae866e55be41
Ali anjo <69678339+alianjo@users.noreply.github.com>
2021-10-15 11:45:15 +0330

    Update replacing.sh

--- replacing.sh
+++ replacing.sh
@@ -1,7 +1,7 @@
 echo -n "Enter your path file: "
 read -r path
-[ -f $path ] 
-if [ $? -ne 0 ] ; then
+[[ -f $path ]] 
+if [[ $? -ne 0 ]] ; then
 	echo "file $path does not exist"
 	exit 1
 fi
@@ -10,7 +10,7 @@ read -r text
 echo -n "Enter your Alternative : "
 read -r sub
 sed -i "s/$text/$sub/g" $path
-if [ $? -eq 0 ] 
+if [[ $? -eq 0 ]] 
 then
 	echo  successful
 fi
